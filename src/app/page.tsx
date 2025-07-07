cd09514f0752bd9476a41031ffaa63e91c8a0f93
Furkan-Kose <furkankose2003@gmail.com>
2025-07-07 15:41:54 +0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ProductList from '@/components/ProductList';
 
 
 async function fetchProducts() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || ''}/api/products`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`, {
     next: { revalidate: 3600 }, 
   });
 
