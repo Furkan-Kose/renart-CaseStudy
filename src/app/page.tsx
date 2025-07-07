22e12785f6b1d0bd788905ef62bc565ddf5df2b3
Furkan-Kose <furkankose2003@gmail.com>
2025-07-07 15:59:37 +0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,41 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import ProductList from '@/components/ProductList';
+import { Product } from '@/types/product';
+import { Loader2 } from 'lucide-react';
 
+export default function HomePage() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
 
-async function fetchProducts() {
-  const res = await fetch(`https://renartcasestudy.vercel.app/api/products`);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch('/api/products');
+        const data = await res.json();
+        setProducts(data.products);
+      } catch (err) {
+        console.error('Veri alınamadı:', err);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch products');
-  }
-
-  const data = await res.json();
-  return data.products;
-}
-
-export default async function HomePage() {
-  const products = await fetchProducts();
+    fetchProducts();
+  }, []);
 
   return (
     <div className="px-4 py-8 min-h-screen">
-      <h2 className="text-3xl text-center font-avenir mb-6">Product List</h2>
-      <ProductList products={products} />
+      <h2 className="text-4xl text-center font-avenir mb-6">Product List</h2>
+
+      {loading ? (
+        <div className="flex justify-center items-center h-64">
+          <Loader2 className="w-8 h-8 animate-spin text-gray-500" />
+        </div>
+      ) : (
+        <ProductList products={products} />
+      )}
     </div>
   );
 }
